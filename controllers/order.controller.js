cf801c677c6dfe6d10a18502c3e8a5843c3c058c
madjek <90720831+madjek@users.noreply.github.com>
2021-10-31 11:06:02 +0100

    controllers added

---
+++ controllers/order.controller.js
@@ -0,0 +1,146 @@
+const db = require("../models");
+const Order = db.orders;
+
+const OrderController = {}; //Create the object controller
+
+//CRUD end-points Functions
+//-------------------------------------------------------------------------------------
+// Create and Save a new Order
+OrderController.create = (req, res) => {
+  // Validate request
+  if (!req.body.type) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
+
+  // Create a Order
+  const order = new Order({
+    type: req.body.type,
+    age: req.body.age
+  });
+
+  // Save order in the database
+  order
+    .save(order)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Order."
+      });
+    });
+};
+
+
+//-------------------------------------------------------------------------------------
+// Retrieve all orders from the database.
+OrderController.findAll = (req, res) => {
+  const type = req.query.type;
+  var condition = type ? { type: { $regex: new RegExp(type), $options: "i" } } : {};
+
+  Order.find(condition)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving orders."
+      });
+    });
+};
+
+
+//-------------------------------------------------------------------------------------
+// Find a single Order with an id
+OrderController.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Order.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Order with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Order with id=" + id });
+    });
+};
+
+
+//-------------------------------------------------------------------------------------
+// Update a Order by the id in the request
+OrderController.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+
+  const id = req.params.id;
+
+  Order.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Order with id=${id}. Maybe Order was not found!`
+        });
+      } else res.send({ message: "Order was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Order with id=" + id
+      });
+    });
+};
+
+
+//-------------------------------------------------------------------------------------
+// Delete a Order with the specified id in the request
+OrderController.delete = (req, res) => {
+  const id = req.params.id;
+
+  Order.findByIdAndRemove(id, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Order with id=${id}. Maybe Order was not found!`
+        });
+      } else {
+        res.send({
+          message: "Order was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Order with id=" + id
+      });
+    });
+};
+
+
+//-------------------------------------------------------------------------------------
+// Delete all orders from the database.
+OrderController.deleteAll = (req, res) => {
+    Order.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Order were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all Order."
+      });
+    });
+};
+
+
+
+module.exports = OrderController;
---
+++ controllers/user.controller.js
@@ -0,0 +1,160 @@
+const db = require("../models");
+const User = db.users;
+
+const UserController = {}; //Create the object controller
+
+//CRUD end-points Functions
+//-------------------------------------------------------------------------------------
+// Create and Save a new User
+UserController.create = (req, res) => {
+  // Validate request
+  if (!req.body.type) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
+
+  // Create a User
+  const user = new User({
+    type: req.body.type,
+    age: req.body.age
+  });
+
+  // Save user in the database
+  user
+    .save(user)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the User."
+      });
+    });
+};
+
+
+//-------------------------------------------------------------------------------------
+// Retrieve all users from the database.
+UserController.findAll = (req, res) => {
+  const type = req.query.type;
+  var condition = type ? { type: { $regex: new RegExp(type), $options: "i" } } : {};
+
+  User.find(condition)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving users."
+      });
+    });
+};
+
+
+//-------------------------------------------------------------------------------------
+// Find a single User with an id
+UserController.findOne = (req, res) => {
+  const id = req.params.id;
+
+  User.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found User with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving User with id=" + id });
+    });
+};
+
+
+//-------------------------------------------------------------------------------------
+// Update a User by the id in the request
+UserController.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+
+  const id = req.params.id;
+
+  User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update User with id=${id}. Maybe User was not found!`
+        });
+      } else res.send({ message: "User was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating User with id=" + id
+      });
+    });
+};
+
+
+//-------------------------------------------------------------------------------------
+// Delete a User with the specified id in the request
+UserController.delete = (req, res) => {
+  const id = req.params.id;
+
+  User.findByIdAndRemove(id, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete User with id=${id}. Maybe User was not found!`
+        });
+      } else {
+        res.send({
+          message: "User was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete User with id=" + id
+      });
+    });
+};
+
+
+//-------------------------------------------------------------------------------------
+// Delete all users from the database.
+UserController.deleteAll = (req, res) => {
+    User.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} User were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all User."
+      });
+    });
+};
+
+exports.allAccess = (req, res) => {
+  res.status(200).send("Public Content.");
+};
+
+exports.userBoard = (req, res) => {
+  res.status(200).send("User Content.");
+};
+
+exports.adminBoard = (req, res) => {
+  res.status(200).send("Admin Content.");
+};
+
+exports.moderatorBoard = (req, res) => {
+  res.status(200).send("Moderator Content.");
+};
+
+module.exports = UserController;
