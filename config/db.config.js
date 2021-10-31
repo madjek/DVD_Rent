d7338806c06c563c71eb344d815e07da838eff6c
madjek <90720831+madjek@users.noreply.github.com>
2021-10-31 11:03:57 +0100

    configuration added

---
+++ config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb://Eugene:1Jekyna2@192.168.1.138:27017/dvd_rent?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false" //Cadena de conexión mongo db
+};
