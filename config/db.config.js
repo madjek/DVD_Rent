c836ec3455c65dc188169a0d5ee334137d3fff6d
madjek <90720831+madjek@users.noreply.github.com>
2021-11-01 19:56:57 +0100

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb+srv://admin:McMLRN0VcCsRtv4k@dvd-rent.c8mki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  url: "mongodb+srv://admin:McMLRN0VcCsRtv4k@dvd-rent.c8mki.mongodb.net/dvd_rent?retryWrites=true&w=majority"
 };
