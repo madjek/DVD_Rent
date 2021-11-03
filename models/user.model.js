2198378550ec0e24433995edcb77a907d448393b
madjek <90720831+madjek@users.noreply.github.com>
2021-11-03 11:12:53 +0100

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -23,7 +23,7 @@ const UserSchema = new Schema({
   }
 });
 
-UserSchema.methods.comparePassword = (password) => {
+UserSchema.methods.comparePassword = function(password) {
   return bcrypt.compareSync(password, this.hash_password);
 };
 
