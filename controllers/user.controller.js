2911a3b03fe68f19d790eb17612cc044d5fb8d76
madjek <90720831+madjek@users.noreply.github.com>
2021-11-05 12:12:42 +0100

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -46,7 +46,7 @@ UserController.sign_in = (req, res) => {
     if (!user || !user.comparePassword(req.body.password)) {
       return res.status(401).json({ message: 'Authentication failed. Invalid user or password.' });
     }
-    return res.json({user: fullName, user: email, token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, authConfig.secret) });
+    return res.json({fullName: fullName, email: email, token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, authConfig.secret) });
   });
 };
 
