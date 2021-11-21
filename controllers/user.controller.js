9d18e678aca151107aad649396facba6e267a5a8
madjek <90720831+madjek@users.noreply.github.com>
2021-11-21 17:09:17 +0100

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -16,7 +16,7 @@ UserController.register = (req, res) => {
     res.status(400).send({ message: "Email can not be empty!" });
     return;
   };
-  if (req.body.email = User.find({email: email})) {
+  if (req.body.email = User.find({email: req.body.email})) {
     res.status(400).send({ message: "Email already exist!" });
     return;
   };
